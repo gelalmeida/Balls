7981d5fceb9ae6aabeae690833d02764e957b9b3
gelalmeida <gel.almeida@campus.fct.unl.pt>
2025-10-16 23:07:06 +0100

    commit first

---
+++ src/Main.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+final String QUIT_MESS = "EXIT";
+final String BYE_MESS = "Bye";
+final String ERROR = "Invalid command";
+final char HUMAN_PLAYER ='P';
+final char START ='S';
+final char[] POSSIBLE_OPPS_IDS ={'a','b','c','d','e','f','g','h','i'};
+char []track;
+int raceLaps;
+int maxSpeed;
+int numbOfOpponents;
+
+
+
+
+void PlayersPositions(int positionS) {   // refazer isto
+
+    int trackLength = track.length;
+    int positionP = (positionS - 1 + trackLength) % trackLength;
+    track[positionP] = HUMAN_PLAYER;
+    for (int i = 0; i < numbOfOpponents; i++) {
+        int posOpp = (positionP - (i + 1) + trackLength) % trackLength;
+        track[posOpp] = POSSIBLE_OPPS_IDS[i];
+    }
+}
+
+
+
+int startingLineFounder() {    //method that founds where the Starting Line 'S' is
+    int i = 0;
+    while (track[i] != START) {
+       i++;
+    }
+        return i;
+
+}
+
+
+
+void initState(Scanner inp){
+    int positionS = startingLineFounder();
+    PlayersPositions(positionS);
+}
+
+
+void launchRace(Scanner inp){
+    //read inputs to initialize state
+    track=inp.next().toCharArray();//Length off the track and its composition
+    raceLaps = inp.nextInt();// number of laps in the race
+    maxSpeed = inp.nextInt();//The maximum speed a player can have
+    numbOfOpponents = inp.nextInt();//number of  players in the race
+    initState(inp);
+
+}
+
+void execQuit(){
+
+}
+
+
+void execStatus(){
+
+}
+
+
+void execShow(){
+
+}
+
+
+void execAccel(Scanner inp){
+    int moreAccel=inp.nextInt();inp.nextLine();
+
+
+}
+
+void execCommands(Scanner inp) {
+    String option;
+    do {
+        option = inp.next().toUpperCase();
+        switch (option){
+            case"ACCEL"->execAccel(inp);
+            case"SHOW" ->execShow();
+            case"STATUS" ->execStatus();
+            case"QUIT" ->execQuit();
+            default ->System.out.println(ERROR);
+        }
+    } while (!(option.equals(QUIT_MESS)));
+    System.out.println(BYE_MESS);
+}
+
+
+void main(){
+    Scanner inp = new Scanner(System.in);
+    launchRace(inp);
+    execCommands(inp);
+    inp.close();
+}
